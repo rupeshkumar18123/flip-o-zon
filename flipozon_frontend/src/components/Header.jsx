8301d219fc01fd1c572b7f46390cf6050d84259b
Rupesh kumar <rupesh18123143@gmail.com>
2025-03-24 13:56:34 +0530

    work on figma design

--- flipozon_frontend/src/components/Header.jsx
+++ flipozon_frontend/src/components/Header.jsx
@@ -11,6 +11,7 @@ function Header() {
       <div>profile</div>
       <div>flip-o-zon payment gateway</div>
       <div>future implementation</div>
+      <div>from today we will make design for this website on figma</div>
     </div>
   )
 }
