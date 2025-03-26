6b8e8b5fee5f02799915a9fec9bdb6e044fab909
Rupesh kumar <rupesh18123143@gmail.com>
2025-03-26 10:57:21 +0530

    change in head

--- flipozon_frontend/src/components/Header.jsx
+++ flipozon_frontend/src/components/Header.jsx
@@ -1,17 +1,27 @@
 
 import React from 'react'
+import CartIcon from './header_component/CartIcon'
 
 function Header() {
   return (
     <div>
-      <div>home</div>
+      {/* <div>home</div>
       <div>like</div>
       <div>cart</div>
       <div>order</div>
       <div>profile</div>
       <div>flip-o-zon payment gateway</div>
       <div>future implementation</div>
-      <div>from today we will make design for this website on figma</div>
+      <div>from today we will make design for this website on figma</div> */}
+
+      <div>logo or name</div>
+      <div>location</div>
+      <div>search</div>
+      <div>user name or account</div>
+      <div>order or return</div>
+      <div>
+        <CartIcon/>
+      </div>
     </div>
   )
 }
---
+++ flipozon_frontend/src/components/header_component/CartIcon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai"; // Import Cart Icon
+import { useSelector } from "react-redux"; // Import Redux state
+import "Head.css";
+
+const CartIcon = () => {
+  const cartItems = useSelector((state) => state.cart.items); // Get cart items from Redux
+  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0); // Calculate total items
+
+  return (
+    <div className="relative">
+      <AiOutlineShoppingCart size={30} className="text-white cursor-pointer" />
+      {totalItems > 0 && (
+        <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
+          {totalItems}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default CartIcon;
