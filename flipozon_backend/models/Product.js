5322ef80a96bfff0379afd80804846b77201837d
Rupesh kumar <rupesh18123143@gmail.com>
2025-04-07 09:33:30 +0530

    product schema

---
+++ flipozon_backend/models/Product.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        validate(value) {
+            if (value < 0) throw new Error('Price must be a positive number');
+        },
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    images: [
+        {
+            url: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    ratings: {
+        type: Number,
+        default: 0,
+    },
+    reviews: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            rating: {
+                type: Number,
+                required: true,
+            },
+            comment: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
