c2ed2fdc526f77b15d85e6ae4d88c8503e6b248d
Rupesh kumar <rupesh18123143@gmail.com>
2025-04-10 18:35:24 +0530

    make route in server file

--- flipozon_backend/server.js
+++ flipozon_backend/server.js
@@ -1,10 +1,16 @@
 const express =require("express");
+const fs = require("fs");
+const path = require("path");
+const route = require(path.join(routesPath, file));
 const app = express();
 
 app.get("/",(req,res)=>{
     res.send("this is a server backend used for flip-o-zon");
 });
-
+const routesPath = path.join(__dirname, "router");
+fs.readdirSync(routesPath).forEach((file) => {
+    app.use(route);
+});
 app.listen(3000,()=>{
     console.log("server started at port number 3000");
 })
