fecdbe1247eeea84bcd34b475e88a48ee8f9becd
Rupesh kumar <rupesh18123143@gmail.com>
2025-04-13 15:40:10 +0530

    change header:

--- flipozon_frontend/src/components/Header.jsx
+++ flipozon_frontend/src/components/Header.jsx
@@ -5,26 +5,23 @@ import "./style.css"
 
 function Header() {
   return (
-    <div className='header'>
-      {/* <div>home</div>
-      <div>like</div>
-      <div>cart</div>
-      <div>order</div>
-      <div>profile</div>
-      <div>flip-o-zon payment gateway</div>
-      <div>future implementation</div>
-      <div>from today we will make design for this website on figma</div> */}
-
-      <div>logo or name</div>
-      <div>location</div>
-      <div>search</div>
-      <div>user name or account</div>
-      <div>order or return</div>
+    <div className='header flex items-center justify-between bg-blue-500 p-4 text-white'>
+      <div className='text-lg font-bold'>logo or name</div>
+      <div className='text-sm'>location</div>
+      <div className='flex-1 mx-4'>
+      <input 
+        type="text" 
+        placeholder="Search" 
+        className='w-full p-2 rounded border border-gray-300'
+      />
+      </div>
+      <div className='text-sm'>user name or account</div>
+      <div className='text-sm'>order or return</div>
       <div>
-        <CartIcon/>
+      <CartIcon />
       </div>
     </div>
-  )
+    )
 }
 
 export default Header
