9980825839683b474ef889c05c679ffe3b0024b4
Rupesh kumar <rupesh18123143@gmail.com>
2025-04-23 14:33:03 +0530

    add footer

--- flipozon_frontend/src/App.jsx
+++ flipozon_frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <h1>flip-o-zon</h1>
       <Header/>
+      <Footer/>
       <h1>first we will make backend then we will come here</h1>
     </>
   )
